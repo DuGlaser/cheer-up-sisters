7c0da581b96e1a3c813172f8d8cde451c60b9057
DuGlaser <duglaser0618@gmail.com>
2020-03-29 18:37:23 +0900

    :sparkles:ADD sendMessage, createMessage func to discord_message.js

--- util/discord_message.js
+++ util/discord_message.js
@@ -6,6 +6,29 @@ const webhookClient = new Discord.WebhookClient(
   app.get("options").DISCORD_WEBHOOK_TOKEN
 );
 
-const sendMessage = () => webhookClient.send("Webhook test");
+function sendMessage(commitCount) {
+  const embed = createMessage(commitCount);
+  webhookClient.send(embed);
+}
+
+function createMessage(commitCount) {
+  // Customize this for you!
+  switch (true) {
+    case commitCount <= 3:
+      return new Discord.MessageEmbed()
+        .setColor("#0099ff")
+        .setTitle("Some title")
+        .setAuthor(
+          "Some name",
+          "https://i.imgur.com/wSTFkRM.png",
+          "https://discord.js.org"
+        )
+        .setImage("https://i.imgur.com/wSTFkRM.png")
+        .setTimestamp();
+
+    default:
+      break;
+  }
+}
 
 module.exports = { sendMessage };
