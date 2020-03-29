5fc4e3440d8626b25632c08126e205c759d9b6d7
DuGlaser <duglaser0618@gmail.com>
2020-03-29 18:21:02 +0900

    :sparkles:ADD cron schedule to send message

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,13 @@
-const app = require("../app");
+const discord = require("../util/discord_message");
+const Github = require("../util/github");
+const moment = require("moment");
+const cron = require("node-cron");
+
+cron.schedule("0 0 0 * * *", async () => {
+  const date = moment().subtract(1, "days").format("YYYY-MM-DD");
+  const commitCount = await Github.getCommitCount(
+    `${date}T00:00:00`,
+    `${date}T23:59:59`
+  );
+  discord.sendMessage(commitCount);
+});
