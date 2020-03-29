e90f9e824b8a1a39aa058dba5021b28b85319059
DuGlaser <duglaser0618@gmail.com>
2020-03-29 17:47:11 +0900

    :sparkles:ADD getCommitCount func to github.js

---
+++ util/github.js
@@ -0,0 +1,28 @@
+const app = require("../app");
+const axios = require("axios");
+
+const getCommitCount = (from, to) => {
+  return axios({
+    url: "https://api.github.com/graphql",
+    headers: {
+      Authorization: `bearer ${app.get("options").GITHUB_TOKEN}`,
+      Accept: "application/vnd.github.v4.idl",
+    },
+    method: "POST",
+    data: {
+      query: `query { 
+       user(login: "Duglaser") { 
+        contributionsCollection(from: "${from}", to: "${to}") {
+          totalCommitContributions
+        }
+      }
+    }`,
+    },
+  })
+    .then((res) => res.data)
+    .then((res) => {
+      return res.data.user.contributionsCollection.totalCommitContributions;
+    });
+};
+
+module.exports = { getCommitCount };
