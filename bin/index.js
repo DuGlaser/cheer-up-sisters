5a1fe2f45b76e83010a76d02845c69a541062d78
DuGlaser <duglaser0618@gmail.com>
2020-04-02 22:20:39 +0900

    :sparkles:FIX schedule process

--- bin/index.js
+++ bin/index.js
@@ -5,9 +5,26 @@ const cron = require("node-cron");
 
 cron.schedule("0 0 0 * * *", async () => {
   const date = moment().subtract(1, "days").format("YYYY-MM-DD");
-  const commitCount = await Github.getCommitCount(
+  const commitCount = await Github.getCommitCountInDay(
     `${date}T00:00:00`,
     `${date}T23:59:59`
   );
-  discord.sendMessage(commitCount);
+  discord.daysMessage(commitCount);
+});
+
+// this work every sunday.
+cron.schedule("0 0 0 * * 0", async () => {
+  const m = moment();
+  const to = m.subtract(1, "days").format("YYYY-MM-DD");
+  const from = m.subtract(7, "days").format("YYYY-MM-DD");
+  const commitCount = await Github.getCommitCountInWeek(
+    `${from}T00:00:00`,
+    `${to}T23:59:59`
+  );
+
+  const commitCountArray = [];
+  commitCount[0].contributionDays.map((item) => {
+    commitCountArray.push(item.contributionCount);
+  });
+  discord.sendWeeklyMessage(commitCountArray);
 });
